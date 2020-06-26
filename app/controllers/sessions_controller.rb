2762b2fdb8e301a1f14d8057fcabc305f32ae4fd
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-26 09:37:53 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,8 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(:name => params[:user][:name])
+    if @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
 end
